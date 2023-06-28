64eae89be8e2ad9297088c537e0cfb1d65611a22
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-06-28 02:40:42 -0300

    feat: ✨ ensure if no email pass return a error in signup controller

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,6 @@
 export class SignUpController {
   async handle (request: any): Promise<any> {
-    return { body: new Error('Missing param: name'), statusCode: 400 }
+    if (!request.body.name) return { body: new Error('Missing param: name'), statusCode: 400 }
+    if (!request.body.email) return { body: new Error('Missing param: email'), statusCode: 400 }
   }
 }
