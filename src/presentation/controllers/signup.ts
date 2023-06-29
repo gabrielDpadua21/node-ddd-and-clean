058983e2fe9ee1cbbf9390fab537c8bdffadca40
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-06-29 03:19:33 -0300

    feat: ✨ create missing param error

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,10 @@
+import { MissinParamError } from '../errors/missing-param-error'
 import { Request, Response } from '../protocols/http'
 
 export class SignUpController {
   async handle (request: Request): Promise<Response> {
-    if (!request.body.name) return { body: new Error('Missing param: name'), statusCode: 400 }
-    if (!request.body.email) return { body: new Error('Missing param: email'), statusCode: 400 }
+    if (!request.body.name) return { body: new MissinParamError('name'), statusCode: 400 }
+    if (!request.body.email) return { body: new MissinParamError('email'), statusCode: 400 }
     return { body: null, statusCode: 200 }
   }
 }
