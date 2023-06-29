fad86af06e53cd943fb500bba60075966b59cf9a
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-06-29 03:16:00 -0300

    feat: ✨ ensure signup is typing with request and response interfaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,6 +1,9 @@
+import { Request, Response } from '../protocols/http'
+
 export class SignUpController {
-  async handle (request: any): Promise<any> {
+  async handle (request: Request): Promise<Response> {
     if (!request.body.name) return { body: new Error('Missing param: name'), statusCode: 400 }
     if (!request.body.email) return { body: new Error('Missing param: email'), statusCode: 400 }
+    return { body: null, statusCode: 200 }
   }
 }
