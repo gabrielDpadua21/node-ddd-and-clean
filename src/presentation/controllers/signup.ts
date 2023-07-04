e880e64fbce786562836af88bc81ecd1048e7e10
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-07-04 00:06:18 -0300

    feat: ✨ create and inject mail validator in signup controller

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,14 +1,18 @@
-import { MissinParamError } from '../errors/missing-param-error'
+import { InvalidParamError, MissinParamError } from '../errors/missing-param-error'
 import { badRequest, sucess } from '../helpers/http-helper'
 import { Controller } from '../protocols/controllers'
+import { EmailValidator } from '../protocols/email-validator'
 import { Request, Response } from '../protocols/http'
 
 export class SignUpController implements Controller {
+  constructor (private readonly emailValidator: EmailValidator) {}
+
   async handle (request: Request): Promise<Response> {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!request.body[field]) return badRequest(new MissinParamError(field))
     }
+    if (!this.emailValidator.isValid(request.body.email)) return badRequest(new InvalidParamError('email'))
     return sucess({})
   }
 }
--- src/presentation/errors/missing-param-error.ts
+++ src/presentation/errors/missing-param-error.ts
@@ -4,3 +4,10 @@ export class MissinParamError extends Error {
     this.name = 'MissinParamError'
   }
 }
+
+export class InvalidParamError extends Error {
+  constructor (param: string) {
+    super(`Invalid param: ${param}`)
+    this.name = 'InvalidParamError'
+  }
+}
